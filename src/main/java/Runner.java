be9a489e836715a88e7f3c094d693ed04d5f55ec
jamos90 <jamos_98@hotmail.com>
2018-07-11 15:51:51 +0100

    adds getsStudentsOnCourse method

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,5 +1,8 @@
+import db.DBCourse;
 import db.DBHelper;
 import models.*;
+
+import java.util.List;
 
 public class Runner {
 
@@ -9,15 +12,21 @@ public class Runner {
 
         Student student1 = new Student("James", 28, 567, course1);
         DBHelper.save(student1);
+        Student student2 = new Student("Helen", 21, 12, course1);
+        DBHelper.save(student2);
 
         Mentor mentor1 = new Mentor("Bryan");
         DBHelper.save(mentor1);
 
         Lesson lesson1 = new Lesson("Hibernate", 22);
         DBHelper.save(lesson1);
+        Lesson lesson2 = new Lesson("Intellij", 22);
+        DBHelper.save(lesson2);
 
         Instructor instructor1 = new Instructor();
         DBHelper.save(instructor1);
+
+        List<Student> studentsOnCourse1 = DBCourse.getStudentsOnCourse(course1);
 
     }
 
